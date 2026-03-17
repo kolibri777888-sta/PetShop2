d07da522b9d5a462fd8cf4779ef062287304b41d
Balakhonow <gord.balakhonow@gmail.com>
2026-03-17 15:57:25 +0300

    CLOSE #1. Добавьте элементы управления пагинацией в TovarForm

--- PetShop/ProductsForm.Designer.cs
+++ PetShop/ProductsForm.Designer.cs
@@ -33,6 +33,13 @@ namespace PetShop
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFirstPage = new System.Windows.Forms.Button();
+            this.btnPrevPage = new System.Windows.Forms.Button();
+            this.lblPageInfo = new System.Windows.Forms.Label();
+            this.btnNextPage = new System.Windows.Forms.Button();
+            this.btnLastPage = new System.Windows.Forms.Button();
+            this.lblRecordInfo = new System.Windows.Forms.Label();
+            this.lblColorHint = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,7 +53,7 @@ namespace PetShop
             this.dgv.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.dgv.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.dgv.Location = new System.Drawing.Point(0, 181);
-            this.dgv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgv.Margin = new System.Windows.Forms.Padding(2);
             this.dgv.Name = "dgv";
             this.dgv.ReadOnly = true;
             this.dgv.RowHeadersWidth = 51;
@@ -59,7 +66,7 @@ namespace PetShop
             this.btnEdit.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnEdit.Location = new System.Drawing.Point(9, 74);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(2);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(176, 39);
             this.btnEdit.TabIndex = 1;
@@ -72,7 +79,7 @@ namespace PetShop
             this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnDelete.Location = new System.Drawing.Point(583, 128);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(130, 50);
             this.btnDelete.TabIndex = 2;
@@ -85,7 +92,7 @@ namespace PetShop
             this.btnAdd.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnAdd.Location = new System.Drawing.Point(9, 11);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(176, 59);
             this.btnAdd.TabIndex = 3;
@@ -93,18 +100,90 @@ namespace PetShop
             this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // btnFirstPage
+            // 
+            this.btnFirstPage.Location = new System.Drawing.Point(411, 46);
+            this.btnFirstPage.Name = "btnFirstPage";
+            this.btnFirstPage.Size = new System.Drawing.Size(75, 23);
+            this.btnFirstPage.TabIndex = 4;
+            this.btnFirstPage.Text = "<<";
+            this.btnFirstPage.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevPage
+            // 
+            this.btnPrevPage.Location = new System.Drawing.Point(410, 103);
+            this.btnPrevPage.Name = "btnPrevPage";
+            this.btnPrevPage.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevPage.TabIndex = 5;
+            this.btnPrevPage.Text = "<";
+            this.btnPrevPage.UseVisualStyleBackColor = true;
+            // 
+            // lblPageInfo
+            // 
+            this.lblPageInfo.AutoSize = true;
+            this.lblPageInfo.Location = new System.Drawing.Point(311, 22);
+            this.lblPageInfo.Name = "lblPageInfo";
+            this.lblPageInfo.Size = new System.Drawing.Size(94, 13);
+            this.lblPageInfo.TabIndex = 6;
+            this.lblPageInfo.Text = "Страница 1 из 10";
+            this.lblPageInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNextPage
+            // 
+            this.btnNextPage.Location = new System.Drawing.Point(577, 49);
+            this.btnNextPage.Name = "btnNextPage";
+            this.btnNextPage.Size = new System.Drawing.Size(75, 23);
+            this.btnNextPage.TabIndex = 7;
+            this.btnNextPage.Text = ">";
+            this.btnNextPage.UseVisualStyleBackColor = true;
+            // 
+            // btnLastPage
+            // 
+            this.btnLastPage.Location = new System.Drawing.Point(555, 102);
+            this.btnLastPage.Name = "btnLastPage";
+            this.btnLastPage.Size = new System.Drawing.Size(75, 23);
+            this.btnLastPage.TabIndex = 8;
+            this.btnLastPage.Text = ">>";
+            this.btnLastPage.UseVisualStyleBackColor = true;
+            // 
+            // lblRecordInfo
+            // 
+            this.lblRecordInfo.AutoSize = true;
+            this.lblRecordInfo.Location = new System.Drawing.Point(318, 61);
+            this.lblRecordInfo.Name = "lblRecordInfo";
+            this.lblRecordInfo.Size = new System.Drawing.Size(64, 13);
+            this.lblRecordInfo.TabIndex = 9;
+            this.lblRecordInfo.Text = "1-20 из 157";
+            // 
+            // lblColorHint
+            // 
+            this.lblColorHint.AutoSize = true;
+            this.lblColorHint.ForeColor = System.Drawing.Color.Red;
+            this.lblColorHint.Location = new System.Drawing.Point(294, 124);
+            this.lblColorHint.Name = "lblColorHint";
+            this.lblColorHint.Size = new System.Drawing.Size(87, 13);
+            this.lblColorHint.TabIndex = 10;
+            this.lblColorHint.Text = "мало на складе";
+            // 
             // ProductsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(722, 463);
+            this.Controls.Add(this.lblColorHint);
+            this.Controls.Add(this.lblRecordInfo);
+            this.Controls.Add(this.btnLastPage);
+            this.Controls.Add(this.btnNextPage);
+            this.Controls.Add(this.lblPageInfo);
+            this.Controls.Add(this.btnPrevPage);
+            this.Controls.Add(this.btnFirstPage);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.dgv);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "ProductsForm";
             this.ShowIcon = false;
@@ -112,6 +191,7 @@ namespace PetShop
             this.Text = "Товары";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -121,5 +201,12 @@ namespace PetShop
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFirstPage;
+        private System.Windows.Forms.Button btnPrevPage;
+        private System.Windows.Forms.Label lblPageInfo;
+        private System.Windows.Forms.Button btnNextPage;
+        private System.Windows.Forms.Button btnLastPage;
+        private System.Windows.Forms.Label lblRecordInfo;
+        private System.Windows.Forms.Label lblColorHint;
     }
 }
